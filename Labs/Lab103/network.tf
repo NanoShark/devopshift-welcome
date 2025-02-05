598f2322b2d9fdc0557e692b2d27f952fbe30e59
Your Name <you@example.com>
2025-02-05 14:32:40 +0000

    update one more

--- Labs/Lab103/network.tf
+++ Labs/Lab103/network.tf
@@ -1,4 +1,12 @@
 resource "aws_security_group" "sg" {
+
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
  ingress {
    from_port   = 80
    to_port     = 80
