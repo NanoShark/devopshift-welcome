0b72d4ecea0fc8eb14055f8f5bcf15d57269a35f
Your Name <you@example.com>
2025-02-05 14:30:39 +0000

    lab103 update

--- Labs/Lab103/global.tf
+++ Labs/Lab103/global.tf
@@ -1,5 +1,9 @@
 provider "aws" {
-  region = "us-east-1"  # Change this to your region
+ region = var.region
+}
+
+variable "region" {
+ default = "us-east-1"
 }
 
 data "aws_ami" "terraform_ami" {
@@ -22,4 +26,24 @@ output "Michael_ami" {
 }
 output "terraformimage" {
   value = data.aws_ami.terraform_ami.id
+}
+
+variable "ami" {
+ default = "ami-0ecc0e0d5986a576d"
+ }
+
+variable "vm_name" {
+ default = "vm-Michael"
+}
+
+variable "admin_username" {
+ default = "admin-user"
+}
+
+variable "admin_password" {
+ default = "Password123!"
+}
+
+variable "vm_size" {
+ default = "t2.micro"
 }
